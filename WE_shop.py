9cd1c6f5500705dc7d25beb64c5f11abd7b52248
Zehra Vahidy <zvahidy@gmail.com>
2022-11-13 18:22:28 -0800

    updated code

--- WE_shop.py
+++ WE_shop.py
@@ -1,5 +1,3 @@
-# WE Shop Marketplace (WE stand together for Women Empowerment)
-
 # Imports
 import streamlit as st
 from dataclasses import dataclass
@@ -18,22 +16,22 @@ from crypto_wallet import w3, generate_account, get_balance
 # Database of Shirts including their name, digital address, rating and in Ether.
 # A single Ether is currently valued at (look up current value)
 
-WE_database = {
+we_shop_database = {
     "Fundamentals": ["Fundamentals", .05],
-    "The Riviter": ["The Riviter", .06],
+    "Rosie The Riviter": ["Rosie The Riviter", .06],
     "RBG": ["RBG", .07],
-    "Codess": ["Codess", .08]
+    "Codess": ["Codess", .08],
 }
 
 # Create a list of the shirt names/type
-Shirt = ["Fundamentals", "Rosie", "RBG"]
+shirts = ["Fundamentals", "Rosie The Riviter", "RBG", "Codess"]
 
 # Create a get_WE function to display the purchase information from the WE_database
-def get_WE():
+def get_shirts():
     """Display the database of cats to purchase information."""
-    db_list = list(WE_database.values())
+    db_list = list(we_shop_database.values())
 
-    for number in range(len(Shirt)):
+    for number in range(len(shirts)):
         st.write("Name: ", db_list[number][0])
         st.write("Price in Ether: ", db_list[number][1], "eth")
         st.text(" \n")
@@ -42,8 +40,8 @@ def get_WE():
 # Streamlit Code
 
 # Create Streamlit application headings using `st.markdown` to explain this app is for buying shirts
-st.markdown("# WE stand together for Women Empowerment")
-st.markdown("## Buy a WE Tee!")
+st.markdown("# WE stand together for Women Empowerment #")
+st.markdown("## Buy a WE Tee! ##")
 st.text(" \n")
 
 #
@@ -54,28 +52,29 @@ account = generate_account(w3)
 ether = get_balance(w3, account.address)
 
 # Disply the balance of ether in the account
-st.sidebar.markdown("## Your Balance of Ether")
+st.sidebar.markdown("## Your Balance of Ether ##")
 st.sidebar.markdown(ether)
 st.sidebar.markdown("---------")
 
 # Create a select box to chose a Shirt using `st.sidebar.selectbox`
-Shirt = st.sidebar.selectbox('Select a Shirt', Shirt)
+shirt = st.sidebar.selectbox('Select a Shirt', shirts)
 
 #  Create a header using ` st.sidebar.markdown()` to display Shirt name and price.
 st.sidebar.markdown("## Shirt Name and Price")
 
 # Identify the Shirt for purchase by name
-Shirt = WE_database[Shirt][0]
+shirt = we_shop_database[shirt][0]
 
 # Create a variable called `Shirt_price` to retrive the cat price from the `WE_database` using block notation.
-Shirt_price = WE_database[Shirt][1]
+shirt_price = we_shop_database[shirt][1]
 
 # Use a conditional statement using the `if` keyword to check if the selected Shirt can be purchased. This will be done by checking the user's account balance that wishes to make the purchase.
-if Shirt_price <= ether:
-  new_balance = float(ether) - float(Shirt_price)
+if shirt_price <= ether:
+  new_balance = float(ether) - float(shirt_price)
 # Write the Shirt name to the sidebar
-  st.sidebar.write("If you buy", Shirt, "for", Shirt_price, "eth, your account balance will be", new_balance, ".")
-  get_WE()
+  st.sidebar.write("If you buy", shirt, "for", shirt_price, "eth, your account balance will be", new_balance, ".")
+  get_shirts()
 else:
-  st.sidebar.write("With a balance of", ether, "ether, you can't buy", Shirt, "for", Shirt_price, "eth." )
-  get_wE()
+  st.sidebar.write("With a balance of", ether, "ether, you can't buy", shirt, "for", shirt_price, "eth." )
+  get_shirts()
+
